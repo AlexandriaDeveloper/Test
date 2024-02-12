89328e0384e0353ba8f1dc506d5b6de3270dd370
Mohamed Ali Sherif <mcit.alex85@gmail.com>
2024-02-12 22:33:05 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hello, World!    From Master Branch");
-=======
+﻿
 Console.WriteLine("Hello, World! from Stage");
->>>>>>> stage
--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df81f0dd44d4ffba89d58b13d61db277f8f18a7")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14963eba1a7f2b2a78d0bf25190b968adf98f2f")]
+>>>>>>> stage
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
