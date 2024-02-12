814f6c4dea6455b9f0f229b4d83d680e9ca54ccd
Mohamed Ali Sherif <mcit.alex85@gmail.com>
2024-02-12 22:40:14 +0200

    create asmahan

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using test;
+
 Console.WriteLine("Hello, World! from Stage");
+
+Peron Asmahan = new Peron();
+Asmahan.Age = 21;
+Asmahan.Gender = PeronGender.male;
+Asmahan.Name = "Asmahan";
